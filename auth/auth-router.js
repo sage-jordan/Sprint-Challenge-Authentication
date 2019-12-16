55bbb9d7911e129cec89f52f31da06a46558659f
sage-jordan <sagemjrodan@gmail.com>
2019-12-15 21:22:17 -0500

    finished auth-router, added bcryptjs and session

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,84 @@
 const router = require('express').Router();
+const db = require('../database/dbConfig');
+const bcrypt = require('bcryptjs');
+const restricted = require('./authenticate-middleware');
+
+// helpers
+
+
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.pass, 12);
+  user.pass = hash;
+
+  if(user.username && user.password){
+    add(user)
+      .then(saved => {
+        res.status(200).json({saved})
+      })
+      .catch(err => {
+        res.status(500).json({err});
+      }); 
+  } else {
+    res.status(404).json({ message: `Please provide a valid Username and Password to register` });
+  };
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  findBy({username})
+    .first()
+    .then(user => {
+      console.log("User: ", user);
+      if(user && bcrypt.compareSync(pass, user.pass)){
+          req.session.user = user;
+          res.status(200).json({ message: `Welcome ${user.username}!`});
+      } else {
+          res.status(401).json({ message: `Invalid Cridentials` });
+      }
+  })
+  .catch(err => {
+      res.status(500).json({ err: err.message })
+  })
 });
 
 module.exports = router;
+
+//helpers 
+
+function find(){
+  return db('users')
+      .select('id', 'username', 'pass');
+};
+
+function findBy(filter){
+  return db('users')
+      .select('id', 'username', 'pass')
+      .where(filter);
+};
+
+function add(user){
+  return db('users')
+      .insert(user)
+      .then(ids => {
+          const [id] = ids;
+          return findById(id);
+      });
+};
+
+function findById(id){
+  return db('users')
+      .select('id', 'username')
+      .where({ id })
+      .first();
+};
+
+function remove(id){
+  return db('users')
+      .where({ id: Number(id) })
+      .del()
+}
